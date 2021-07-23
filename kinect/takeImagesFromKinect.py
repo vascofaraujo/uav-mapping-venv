e61740d4f69f2e0dae3a28fac3f1c75a648167f6
vascofaraujo <vasco.faden.araujo@gmail.com>
2021-07-23 23:27:25 +0100

    more images

--- kinect/takeImagesFromKinect.py
+++ kinect/takeImagesFromKinect.py
@@ -14,27 +14,28 @@ def get_depth():
     array = array.astype(np.uint8)
     return array
 
-while True:
-    frame = get_video()
-    depth = get_depth()
-
+if __name__ == '__main__':
     i = 0
+    while True:
+        frame = get_video()
+        depth = get_depth()
 
-    # nameImg = str(time.time()) + ".jpg"
-    #Save image when "q" is pressed
-    if cv.waitKey(30) & 0xFF == ord('q'):
-        cv.imwrite("img-" + str(i) + ".png",frame)
-        cv.imwrite("depth-" + str(i) + ".png",depth)
-        i += 1
-        print("Captured")
-        cv.waitKey(0)
 
-    #Show webcam
-    cv.imshow("img",frame)
-    cv.imshow("d",depth)
-    #Increment i for name of image to be saved
-    #i = i + 1
+        # nameImg = str(time.time()) + ".jpg"
+        #Save image when "q" is pressed
+        if cv.waitKey(30) & 0xFF == ord('q'):
+            cv.imwrite("img-" + str(i) + ".png",frame)
+            cv.imwrite("depth-" + str(i) + ".png",depth)
+            i += 1
+            print("Captured")
+            cv.waitKey(0)
 
-    #break on ESC
-    if cv.waitKey(30) & 0xFF == 27:
-        break
+        #Show webcam
+        cv.imshow("img",frame)
+        cv.imshow("d",depth)
+        #Increment i for name of image to be saved
+        #i = i + 1
+
+        #break on ESC
+        if cv.waitKey(30) & 0xFF == 27:
+            break
