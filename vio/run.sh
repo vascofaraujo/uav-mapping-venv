a26c8e13ad8127c60fb4093d60a8e1524626d075
Renato B.L <renato.loureiro@tecnico.ulisboa.pt>
2021-08-13 23:45:43 +0100

    correct instructions

--- vio/run.sh
+++ vio/run.sh
@@ -1,1 +1,2 @@
-~/Documents/git-repo/LARVIO/build/larvio ~/Documents/git-repo/LARVIO/MH_01/mav0/imu0/data.csv ~/Documents/git-repo/LARVIO/MH_01/mav0/cam0/data.csv ~/Documents/git-repo/LARVIO/MH_01/mav0/cam0/data ~/LARVIO/config/euroc.yaml
+~/uav-mapping-venv/vio/build/larvio ~/uav-mapping-venv/vio/data_sets/MH_01/mav0/imu0/data.csv ~/uav-mapping-venv/vio/data_sets/MH_01/mav0/cam0/data.csv ~/uav-mapping-venv/vio/data_sets/MH_01/mav0/cam0/data ~/uav-mapping-venv/vio/config/euroc.yaml
+
